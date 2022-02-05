479a2872043ea099a246b13c697dde54e925d94f
Uwaise Ibna Islam <uwaiseibna@outlook.com>
2022-02-05 20:41:52 +0600

    new

---
+++ left_rotate_arr_n_times.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void rotate_arr_n_times(int a[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        int n,b[size];
+        cout<<"enter the value of n: ";
+        cin>>n;
+        for(int i=0;i<size;i++)
+        {
+            b[i]=a[(i+n)%size];
+        }
+        a=b;
+        for(int i=0;i<size;i++)
+        {
+            cout<< a[i]<<endl;
+        }
+    }
+}
+
+main()
+
+{
+    int a[]={1,2,3,4,5,6};
+    int size=sizeof(a)/sizeof(a[0]);
+    rotate_arr_n_times(a,size);
+}
